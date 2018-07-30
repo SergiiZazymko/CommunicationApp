780de83691ea4e95543eb591b592b591cb759371
Sergiy Zazymko <sergiy.zazymko@gns-it.com>
2018-07-30 11:52:06 +0300

    Add jQuery

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
 chdir(dirname(__DIR__));
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
